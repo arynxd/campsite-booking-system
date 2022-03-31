113c656d7b833e67eb6552aa35da9bc9f6fd4b3f
arynxd <64087640+arynxd@users.noreply.github.com>
2022-03-31 13:36:35 +0100

    feat(frontend): scaffold components
    this scaffolds out the frontend components and services that will be used in the project

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { UsersComponent } from './users/users.component'
+import { BookingsComponent } from './bookings/bookings.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
-	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		UsersComponent,
+		BookingsComponent,
+	],
+	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
